d2cebd54b7c2a5877a25aae7122fdd39c6dc6b9e
cristiansiffu <csiffu96@gmail.com>
2022-12-12 16:08:58 +0100

    first commit

---
+++ src/main/java/com/Segnalazioni/Covid/SegnalazioniCovidApplication.java
@@ -0,0 +1,13 @@
+package com.Segnalazioni.Covid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegnalazioniCovidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegnalazioniCovidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Segnalazioni/Covid/SegnalazioniCovidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Segnalazioni.Covid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegnalazioniCovidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

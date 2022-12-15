75f3310d845a81ccf2a6187a6209ce6eded79005
SaraMottolese <saramottolese89@yahoo.it>
2022-12-15 17:36:39 +0100

    report and path service implementation

--- src/main/java/com/Segnalazioni/Covid/service/Impl/PathServiceImpl.java
+++ src/main/java/com/Segnalazioni/Covid/service/Impl/PathServiceImpl.java
@@ -2,15 +2,23 @@ package com.Segnalazioni.Covid.service.Impl;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Segnalazioni.Covid.exception.SegnalazioniException;
 import com.Segnalazioni.Covid.model.Path;
+import com.Segnalazioni.Covid.repository.PathRepository;
 import com.Segnalazioni.Covid.service.PathService;
-
+@Service
 public class PathServiceImpl implements PathService {
-
+@Autowired
+private PathRepository pathRepo;
 	@Override
-	public Optional<Path> findById(Long id) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+	public Path findById(Long id) {
+		Optional <Path> path= pathRepo.findById(id);
+		if(path.isEmpty())
+			throw new SegnalazioniException("Percorso nnon trovato");
+		return path.get();
 	}
 
 }
--- src/main/java/com/Segnalazioni/Covid/service/Impl/ReportServiceImpl.java
+++ src/main/java/com/Segnalazioni/Covid/service/Impl/ReportServiceImpl.java
@@ -5,27 +5,39 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
+import org.springframework.stereotype.Service;
 
+import com.Segnalazioni.Covid.exception.SegnalazioniException;
 import com.Segnalazioni.Covid.model.Report;
 import com.Segnalazioni.Covid.model.TypeOfReport;
+import com.Segnalazioni.Covid.repository.ReportRepository;
 import com.Segnalazioni.Covid.service.ReportService;
-
+@Service
 public class ReportServiceImpl implements ReportService {
-
+@Autowired
+private ReportRepository reportRepo;
 	@Override
 	public Report add(Report report) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Report> newReport = reportRepo.findById(report.getIdReport());
+		if(!newReport.isPresent())
+			return reportRepo.save(report);
+		else
+			throw new SegnalazioniException("report gia' esistente");
 	}
 
 	@Override
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
+		Optional<Report> reportResult= reportRepo.findById(id);
+		if(reportResult.isPresent())
+			reportRepo.deleteById(id);
+		else
+			throw new SegnalazioniException("Impossibile eliminare il report");
 		
 	}
 
